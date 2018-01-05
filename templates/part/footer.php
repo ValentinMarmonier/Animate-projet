3622e9ea1e2935e7d39dca04c8c5cb7151576d48
Valentin Marmonier <valentin.marmonier@free.fr>
2018-01-05 09:20:26 +0000

    maj2

--- templates/part/footer.php
+++ templates/part/footer.php
@@ -1,8 +1,81 @@
-
         </main>
+        
         <footer>
-            <p>tous droits réservés</p>
+            <section id="cadre-TT">
+                <h4 id="titre-cadre-ZZ">Titre h4 :<span id="suite-titre-cadre-WW">Suite titre h4</span></h4>
+                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p>
+                <div id="cta-upercase" class="cta-upercase">
+                    <a class="AA" href="/customer-intelligence">En savoir plus</a><span class="BB">&nbsp;&#8594;</span>
+                </div>    
+            </section><!-- section#cadre-? -->
+
+            <section id="ressources-tutos" class="">
+                <h4 id="titre-ress-tut"><span>Ressources et tutoriels</span></h4>
+                <div id="cta-lowercase" class="cta-lowercase">
+                    <a class="" href=""><pan>+</pan></a>
+                </div>
+            </section><!-- section#ressources-tutos -->
+
+            <section class="bottom-footer">
+                <div class="column-1">
+                    <div id="contact">
+                        <a  href="contact"><span>Contact</span></a>
+                    </div>
+                    <div id="infos-legales">
+                        <a s href="infos-legales"><span>Infos légales</span></a>
+                    </div>
+                    <div id="cgu">
+                        <a href="cgu"><span>Conditions générales d'utilisation</span></a>
+                    </div>
+                    <div id="cgv">
+                        <a href=""><span>Conditions générales de vente</span></a>
+                    </div>
+                </div><!-- .column-1 -->
+
+                <div class="column-2">
+                    <div id="accueil-footer">
+                        <a  href="accueil"><span>Accueil</span></a>
+                    </div>
+                    <div id="produits-footer">
+                        <a href="produits"><span>Produits</span></a>
+                    </div>
+                    <div id="blog-footer">
+                        <a href="blog"><span>Blog</span></a>
+                    </div>
+                    <div id="boutique">
+                        <a href="boutique"><span>Boutique</span></a>
+                    </div>
+                </div><!-- .column-2 -->
+
+                <div class="column-3">
+                    <adresse>
+                        <div id="tel">
+                            <a href="tel:+33608133351"> Appeler la société Animate&reg; </a>
+                        </div>
+                        <div id="email">
+                            <a href="mailto:eric.henryon@animate.fr"> Envoyer un email </a>
+                        </div>
+                        <div id="bureaux">
+                            Technopôle de Château Gombert<br>
+                            Hotel Technoptic<br>
+                            2, rue Marc Donadille<br>
+                            13 013 Marseille
+                        </div>
+                    </adresse>
+                </div><!-- .column-3 -->
+
+                <div class="column-4">
+                    <a class="social" href="https://www.facebook.com/Animate.figurine/" alt="facebook">  
+                    </a>
+                    <a class="social" href="https://www.youtube.com/channel/UC1alssxte9DNlCR_6N9SIxg/" alt="youtube">   
+                    </a>
+                    <a class="social" href="https://www.instagram.com/figurineanimate/" alt="instagram">
+                    </a>
+                </div><!-- .column-4 -->
+ 
+            </section><!-- section.bottom-footer -->
+
         </footer>
-        <script type="text/javascript" src="<?php echo $urlAccueil ?>assets/js/script.js"></script>
+
     </body>
 </html>
--- templates/part/header.php
+++ templates/part/header.php
@@ -33,3 +33,84 @@ $urlPictogramme     = $this->generateUrl("pictogramme");
             </nav>
         </header>
         <main>
+            
+
+<?php
+
+// ON RECUPERE LES URLS DES PAGES GRACE AU NOM DE LEUR ROUTE
+$urlAccueil         = $this->generateUrl("accueil");
+$urlproduits        = $this->generateUrl("produits");
+$urlBlog            = $this->generateUrl("blog");
+$urlBoutique        = $this->generateUrl("boutique");
+$urlAdminLogin      = $this->generateUrl("adminLogin");
+$urlEspaceAdmin     = $this->generateUrl("espaceAdmin");
+$urlKit             = $this->generateUrl("kit");
+$urlCategorie       = $this->generateUrl("categorieGlob");
+$urlPictogramme     = $this->generateUrl("pictogramme");
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="fr-FR" prefix="og: http://ogp.me/ns#" class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html lang="fr-FR" prefix="og: http://ogp.me/ns#" class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html lang="fr-FR" prefix="og: http://ogp.me/ns#" class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html lang="fr-FR" prefix="og: http://ogp.me/ns#" class="no-js"><!--<![endif]-->
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="format-detection" content="telephone=no">
+
+        <title>Animate&reg;, outil d&#039;aide à la communication et de rééducation</title>
+
+        <!-- POUR DISPOSER DU CHEMIN ABSOLU A PARTIR DE LA PAGE Accueil -->
+        
+        <link rel="stylesheet" href="css/style-index.css" type="text/css" />
+        
+       
+        <meta name="description" content="Animate, outil de communication, de rééducation pour l&#039;autisme, la déficience intellectuelle, les troubles neuros-dégénératifs et des apprentissages" />
+        <meta name="robots" content="noodp"/>
+        <link rel="canonical" href="http://www.animate.fr/" />
+        <meta property="og:locale" content="fr_FR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Animate&reg;, outil d&#039;aide à la communication et de rééducation" />
+        <meta property="og:description" content="Structure unique de marketing pour piloter le développement du business et la montée en compétences des marques" />
+        <meta property="og:url" content="http://www.animate.fr/" />
+        <meta property="og:site_name" content="Animate | Outil de remédiation pour la prise en charge des troubles du langage et du comportement" />
+    </head>
+    
+    <body>
+        
+        <header id="site-header" class="" role="banner">
+            <a href="https://www.animate.fr/">
+                <img src="img/logo-nav.png" alt="Logo Animate | Outil de remédiation pour la prise en charge des troubles du langage et du comportement" class="default-logo" id="logo-nav" /> 
+            </a>                    
+        
+            <!-- #site-navigation -->
+            <nav id="site-navigation" class="from-top" role="navigation">
+                <div class="menu-container">
+                    <ul id="menu-nav" class="menu">
+                        <li class="">
+                            <a href="accueil">Accueil</a>
+                        </li>
+                        <li class="">
+                            <a href="produits">Produits</a>
+                        </li>
+                        <li class="">
+                            <a href="blog">Blog</a>
+                        </li>
+                        <li class="">
+                            <a href="boutique">Boutique</a>
+                        </li>
+                    </ul>           
+                </div><!-- .menu-container -->
+            </nav><!-- #site-navigation -->                   
+
+        </header><!-- #site-header -->
+
+        <main>
+        </main>
+
+
+ 
+
+        
