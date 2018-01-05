690a97468dc07a93f339c182bfd5e754b46b13fb
Valentin Marmonier <valentin.marmonier@free.fr>
2018-01-05 12:55:53 +0000

    Maj Controller

--- src/Controller/VitrineController.php
+++ src/Controller/VitrineController.php
@@ -44,6 +44,166 @@ class VitrineController
        
    }
    
+   /**
+      * @Route("video/", name="video")
+      */   
+   public function video (Request $objetRequest)
+   {
+        // POUR RECUPERER LES INFOS DE FORMULAIRE
+        // ON DEMANDE A SYMFONY DE NOUS FOURNIR 
+        // UN OBJET DE LA CLASSE Request
+        // (INJECTION DE DEPENDANCE...)
+       
+        // JE VAIS METTRE EN CACHE LE CODE HTML
+        // http://php.net/manual/fr/function.ob-start.php
+        ob_start();
+        
+        // METHODE DE SYMFONY POUR OBTENIR LE CHEMIN DU DOSSIER symfony1        
+        $cheminSymfony   = $this->getParameter('kernel.project_dir');
+        $cheminTemplates = "$cheminSymfony/templates"; 
+        $cheminPart      = "$cheminTemplates/part"; 
+        require_once("$cheminTemplates/template-video.php");
+        
+        // RECUPERER LE CONTENU DU CACHE
+        // http://php.net/manual/fr/function.ob-get-clean.php
+        $contenuCache = ob_get_clean();
+        
+        // TEMPORISATION DE L'AFFICHAGE...
+        // JE NE FAIS PAS LE echo DE L'AFFICHAGE MOI MEME
+        // JE DONNE LE CONTENU HTML A LA CLASSE Response
+        // ET C'EST LA MECANIQUE DE SYMFONY QUI VA GERER L'AFFICHAGE DE CE CODE
+        return new Response($contenuCache);
+       
+   }
+   
+   /**
+      * @Route("fondateurs/", name="fondateurs")
+      */   
+   public function fondateurs (Request $objetRequest)
+   {
+        // POUR RECUPERER LES INFOS DE FORMULAIRE
+        // ON DEMANDE A SYMFONY DE NOUS FOURNIR 
+        // UN OBJET DE LA CLASSE Request
+        // (INJECTION DE DEPENDANCE...)
+       
+        // JE VAIS METTRE EN CACHE LE CODE HTML
+        // http://php.net/manual/fr/function.ob-start.php
+        ob_start();
+        
+        // METHODE DE SYMFONY POUR OBTENIR LE CHEMIN DU DOSSIER symfony1        
+        $cheminSymfony   = $this->getParameter('kernel.project_dir');
+        $cheminTemplates = "$cheminSymfony/templates"; 
+        $cheminPart      = "$cheminTemplates/part"; 
+        require_once("$cheminTemplates/template-fondateurs.php");
+        
+        // RECUPERER LE CONTENU DU CACHE
+        // http://php.net/manual/fr/function.ob-get-clean.php
+        $contenuCache = ob_get_clean();
+        
+        // TEMPORISATION DE L'AFFICHAGE...
+        // JE NE FAIS PAS LE echo DE L'AFFICHAGE MOI MEME
+        // JE DONNE LE CONTENU HTML A LA CLASSE Response
+        // ET C'EST LA MECANIQUE DE SYMFONY QUI VA GERER L'AFFICHAGE DE CE CODE
+        return new Response($contenuCache);
+       
+   }
+   
+   /**
+      * @Route("benefices/", name="benefices")
+      */   
+   public function benefices (Request $objetRequest)
+   {
+        // POUR RECUPERER LES INFOS DE FORMULAIRE
+        // ON DEMANDE A SYMFONY DE NOUS FOURNIR 
+        // UN OBJET DE LA CLASSE Request
+        // (INJECTION DE DEPENDANCE...)
+       
+        // JE VAIS METTRE EN CACHE LE CODE HTML
+        // http://php.net/manual/fr/function.ob-start.php
+        ob_start();
+        
+        // METHODE DE SYMFONY POUR OBTENIR LE CHEMIN DU DOSSIER symfony1        
+        $cheminSymfony   = $this->getParameter('kernel.project_dir');
+        $cheminTemplates = "$cheminSymfony/templates"; 
+        $cheminPart      = "$cheminTemplates/part"; 
+        require_once("$cheminTemplates/template-benefices.php");
+        
+        // RECUPERER LE CONTENU DU CACHE
+        // http://php.net/manual/fr/function.ob-get-clean.php
+        $contenuCache = ob_get_clean();
+        
+        // TEMPORISATION DE L'AFFICHAGE...
+        // JE NE FAIS PAS LE echo DE L'AFFICHAGE MOI MEME
+        // JE DONNE LE CONTENU HTML A LA CLASSE Response
+        // ET C'EST LA MECANIQUE DE SYMFONY QUI VA GERER L'AFFICHAGE DE CE CODE
+        return new Response($contenuCache);
+       
+   }
+   
+   /**
+      * @Route("partenaires/", name="partenaires")
+      */   
+   public function partenaires (Request $objetRequest)
+   {
+        // POUR RECUPERER LES INFOS DE FORMULAIRE
+        // ON DEMANDE A SYMFONY DE NOUS FOURNIR 
+        // UN OBJET DE LA CLASSE Request
+        // (INJECTION DE DEPENDANCE...)
+       
+        // JE VAIS METTRE EN CACHE LE CODE HTML
+        // http://php.net/manual/fr/function.ob-start.php
+        ob_start();
+        
+        // METHODE DE SYMFONY POUR OBTENIR LE CHEMIN DU DOSSIER symfony1        
+        $cheminSymfony   = $this->getParameter('kernel.project_dir');
+        $cheminTemplates = "$cheminSymfony/templates"; 
+        $cheminPart      = "$cheminTemplates/part"; 
+        require_once("$cheminTemplates/template-partenaires.php");
+        
+        // RECUPERER LE CONTENU DU CACHE
+        // http://php.net/manual/fr/function.ob-get-clean.php
+        $contenuCache = ob_get_clean();
+        
+        // TEMPORISATION DE L'AFFICHAGE...
+        // JE NE FAIS PAS LE echo DE L'AFFICHAGE MOI MEME
+        // JE DONNE LE CONTENU HTML A LA CLASSE Response
+        // ET C'EST LA MECANIQUE DE SYMFONY QUI VA GERER L'AFFICHAGE DE CE CODE
+        return new Response($contenuCache);
+       
+   }
+   
+   /**
+      * @Route("cibles/", name="cibles")
+      */   
+   public function cibles (Request $objetRequest)
+   {
+        // POUR RECUPERER LES INFOS DE FORMULAIRE
+        // ON DEMANDE A SYMFONY DE NOUS FOURNIR 
+        // UN OBJET DE LA CLASSE Request
+        // (INJECTION DE DEPENDANCE...)
+       
+        // JE VAIS METTRE EN CACHE LE CODE HTML
+        // http://php.net/manual/fr/function.ob-start.php
+        ob_start();
+        
+        // METHODE DE SYMFONY POUR OBTENIR LE CHEMIN DU DOSSIER symfony1        
+        $cheminSymfony   = $this->getParameter('kernel.project_dir');
+        $cheminTemplates = "$cheminSymfony/templates"; 
+        $cheminPart      = "$cheminTemplates/part"; 
+        require_once("$cheminTemplates/template-cibles.php");
+        
+        // RECUPERER LE CONTENU DU CACHE
+        // http://php.net/manual/fr/function.ob-get-clean.php
+        $contenuCache = ob_get_clean();
+        
+        // TEMPORISATION DE L'AFFICHAGE...
+        // JE NE FAIS PAS LE echo DE L'AFFICHAGE MOI MEME
+        // JE DONNE LE CONTENU HTML A LA CLASSE Response
+        // ET C'EST LA MECANIQUE DE SYMFONY QUI VA GERER L'AFFICHAGE DE CE CODE
+        return new Response($contenuCache);
+       
+   }
+   
    
    
 
