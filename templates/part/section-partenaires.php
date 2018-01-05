de0096893abd75e69f38f56c5e3c14e29f0d2230
Dominique GENTY <dominiquegenty.philantroc@gmail.com>
2018-01-05 15:06:13 +0000

    maj dom

---
+++ templates/part/section-partenaires.php
@@ -0,0 +1,35 @@
+            <section id="partenaires" class="sections-index">
+                <div id="icones-partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                </div>
+                
+                <div id="logos-partenaires">
+                    <h2 id="titre-partenaires">Partenaires</h2>
+                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
+                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
+                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
+                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
+                </div>
+                
+                <div id="centre-IME">
+                    <h3 id="titre-IME"></h3>Vous êtes centre IME
+                    <p class="paragraphes-index">
+                        Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset 
+                        contenant des passages du Lorem Ipsum, et, plus récemment, 
+                        par son inclusion dans des applications de mise en page de texte, 
+                        comme Aldus PageMaker.</p> 
+                </div>
+                <div id="cta-upercase" class="cta-upercase">
+                     <a class="bouton cta-bouton" href="#">Bouton cta</a>
+                </di>
+            </section>     
