f9f7a6e0dacf0457ebdd9c93a028d2c3a6ce6872
Thibaut Vivant <thibaut.vivant@gmail.com>
2018-01-05 13:20:20 +0000

    fondateur et video

---
+++ templates/part/section-fondateurs.php
@@ -0,0 +1,28 @@
+<div class="fondateurs">
+    <div class="cadre">
+        <img src="img/eric-yunshan.png"></img>
+    </div>
+                    <div class="presentation">
+                        <h4>Cher visiteur</h4>
+                        <article>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                        magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                        quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                        ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+                        dolor in hendrerit in vulputate velit esse molestie consequat,
+                        vel illum dolore eu feugiat nulla facilisis at vero eros et
+                        accumsan et iusto odio dignissim qui blandit praesent luptatum
+                        zzril delenit augue duis dolore te feugait nulla facilisi.
+                        
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                        magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                        quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                        ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+                        dolor in hendrerit in vulputate velit esse molestie consequat,
+                        vel illum dolore eu feugiat nulla facilisis at vero eros et
+                        accumsan et iusto odio dignissim qui blandit praesent luptatum
+                        zzril delenit augue duis dolore te feugait nulla facilisi.
+                      </article>
+                    </div>
+                </div>
---
+++ templates/part/section-video.php
@@ -0,0 +1,7 @@
+<div class="intro">
+    <img class="gif" src="img/mettre-carte-new.gif" loop=infinite></img>
+        <div class="video">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/qskzSUsuLeU"
+            frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+        </div>
+</div>
