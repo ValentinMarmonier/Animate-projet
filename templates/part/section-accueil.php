eaefb81babb3efc986b7a449c76aa8ced99dc9b7
Thibaut Vivant <thibaut.vivant@gmail.com>
2018-01-08 01:47:30 +0000

    section troubles

--- templates/part/section-accueil.php
+++ templates/part/section-accueil.php
@@ -3,5 +3,6 @@
 require_once("$cheminPart/header.php");
 require_once("$cheminPart/section-video.php");
 require_once("$cheminPart/section-fondateurs.php");
+require_once("$cheminPart/section-troubles.php");
 require_once("$cheminPart/footer.php");
     
--- templates/part/section-troubles.php
+++ templates/part/section-troubles.php
@@ -1,39 +1,39 @@
 <section class="sectionTroubles">
     <h3>AniMate aide à travailler sur différents troubles</h3>
    
-    <div>
-        <img src="../../src/assets/img/cognition-sociale-1-1024x900.jpg" alt="traiter l'autisme"></img>
-        <p>
+    <div class="troublesdroite">
+         <p>
             Autisme
         </p>
+        <img src="../src/assets/img/cognition-sociale-1-1024x900.jpg" alt="traiter l'autisme"></img>
     </div>
     
-    <div>
-        <img src="../../src/assets/img/exemple-Emotions-1024x900.jpg" alt="Déficience intellectuelle"></img>
+    <div class="troublesgauche">
+        <img src="../src/assets/img/exemple-Emotions-1024x900.jpg" alt="Déficience intellectuelle"></img>
         <p>
             Déficience intellectuelle
         </p>
     </div>  
     
-    <div>
-        <img src="../../src/assets/img/meteo3-1024x900.jpeg" alt="Troubles Dys"></img>
-        <p>
+    <div class="troublesdroite">
+                <p>
             Troubles Dys
         </p>
+        <img src="../src/assets/img/meteo3-1024x900.jpeg" alt="Troubles Dys"></img>
     </div>
     
-    <div>
-        <img src="../../src/assets/img/hygiene-1024x900.jpg" alt="Maladies neurodégénératives"></img>
+    <div class="troublesgauche">
+        <img src="../src/assets/img/hygiene-1024x900.jpg" alt="Maladies neurodégénératives"></img>
         <p>
             Maladies neurodégénératives
         </p>
     </div>
     
-    <div>
-        <img src="../../src/assets/img/cognitive-1024x900.png" alt="Troubles du comportement"></img>
+    <div class="troublesdroite">
         <p>
             Troubles du comportement
         </p>
+        <img src="../src/assets/img/cognitive-1024x900.png" alt="Troubles du comportement"></img>
     </div>
     
     
