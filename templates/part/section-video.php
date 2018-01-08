353af473b93cd65fd611992ed8a6f2d6c682ec29
Thibaut Vivant <thibaut.vivant@gmail.com>
2018-01-08 01:09:48 +0000

    partie video et debut fondateurs

--- templates/part/section-video.php
+++ templates/part/section-video.php
@@ -1,7 +1,15 @@
-<div class="intro">
-    <img class="gif" src="../../src/assets/img/mettre-carte-new.gif" loop=infinite></img>
+
+<section>
+    <h3>SECTION video</h3>
+</section>
+
+<section>
+    <div class="intro">
+    <img class="gif" src="../src/assets/img/coeur.gif" loop=infinite></img>
         <div class="video">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/qskzSUsuLeU"
             frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
         </div>
 </div>
+    
+</section>
