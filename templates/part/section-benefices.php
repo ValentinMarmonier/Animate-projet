8771a8efe7dbaa01c558e0e3948c65edd2ad3c07
Dominique GENTY <dominiquegenty.philantroc@gmail.com>
2018-01-08 13:11:30 +0000

    maj

--- templates/part/section-benefices.php
+++ templates/part/section-benefices.php
@@ -6,23 +6,8 @@
                 <p class="paragraphe-benefices">Le Lorem Ipsum est simplement du faux texte employé dans la composition 
                 et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
                 quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
-                <div class="ligne-benefices">
-                    <h3 class="titre-3-sections">Titre h3 ligne 1 section bénéfices</h3>
-                    <p class="paragraphes-benefices" lass="paragraphes-index"> 
-                    Il n'a pas fait que survivre cinq siècles,<br>
-                    mais s'est aussi adapté à la bureautique informatique,<br> 
-                    sans que son contenu n'en soit modifié</p>
-                    <img class="icones-benefices" src="img/" alt="">
-                </div>
-                <div class="ligne-benefices">
-                    <img class="icones-benefices" src="img/" alt="">
-                    <h3 class="titre-3-sections">Titre h3 ligne 2 section bénéfices</h3>
-                    <p class="paragraphes-benefices" lass="paragraphes-index"> 
-                    Il n'a pas fait que survivre cinq siècles,<br>
-                    mais s'est aussi adapté à la bureautique informatique,<br> 
-                    sans que son contenu n'en soit modifié</p>
-                </div>
-                <div class="ligne-benefices">
+                
+                <div class="ligne-benefices" class="lignes-index">
                     <h3 class="titre-3-sections">Titre h3 ligne 1 section bénéfices</h3>
                     <p class="paragraphes-benefices" class="paragraphes-index"> 
                     Il n'a pas fait que survivre cinq siècles,<br>
@@ -30,4 +15,23 @@
                     sans que son contenu n'en soit modifié</p>
                     <img class="icones-benefices" src="img/" alt="">
                 </div>
+                
+                <div class="ligne-benefices" class="lignes-index">
+                    <img class="icones-benefices" src="img/" alt="">
+                    <h3 class="titre-3-sections">Titre h3 ligne 2 section bénéfices</h3>
+                    <p class="paragraphes-benefices" class="paragraphes-index"> 
+                    Il n'a pas fait que survivre cinq siècles,<br>
+                    mais s'est aussi adapté à la bureautique informatique,<br> 
+                    sans que son contenu n'en soit modifié</p>
+                </div>
+                
+                <div class="ligne-benefices" class="lignes-index">
+                    <h3 class="titre-3-sections">Titre h3 ligne 1 section bénéfices</h3>
+                    <p class="paragraphes-benefices" class="paragraphes-index"> 
+                    Il n'a pas fait que survivre cinq siècles,<br>
+                    mais s'est aussi adapté à la bureautique informatique,<br> 
+                    sans que son contenu n'en soit modifié</p>
+                    <img class="icones-benefices" src="img/" alt="">
+                </div>
+                
             </section>
