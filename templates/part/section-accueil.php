0445d8710ff9b037f8be2828aac750ffbed41aec
Thibaut Vivant <thibaut.vivant@gmail.com>
2018-01-08 12:39:17 +0000

    benefices

--- templates/part/section-accueil.php
+++ templates/part/section-accueil.php
@@ -3,6 +3,6 @@
 require_once("$cheminPart/header.php");
 require_once("$cheminPart/section-video.php");
 require_once("$cheminPart/section-fondateurs.php");
-require_once("$cheminPart/section-troubles.php");
+require_once("$cheminPart/section-benefices.php");
 require_once("$cheminPart/footer.php");
     
