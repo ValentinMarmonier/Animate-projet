fc3a9d0de6e6276840a608677688b223b6d75b1c
Thibaut Vivant <thibaut.vivant@gmail.com>
2018-01-08 14:12:46 +0000

    cta

--- templates/part/section-partenaires.php
+++ templates/part/section-partenaires.php
@@ -1,28 +1,32 @@
             <section id="partenaires" class="sections-index">
                 <div id="icones-partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
-                    <img class="icones-partenaires" src="img/" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    </div>
+                    <div id="icones-partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
+                    <img class="icones-partenaires" src="http://www.renders-graphiques.fr/image/upload/normal/5353_render_logo_om.png" alt="icone ... partenaires">
                 </div>
-                
-                <div id="logos-partenaires">
+                <div id="partenaires">
                     <h2 id="titre-partenaires">Partenaires</h2>
-                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
-                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
-                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
-                    <img class="logo-partenaires" src="img/" alt="logo ... partenaires">
+                    </div>
+                <div id="logos-partenaires">
+                    
+                    <img class="logo-partenaires" src="https://upload.wikimedia.org/wikipedia/fr/thumb/8/82/Ville_de_Marseille_%28logo%29.svg/1280px-Ville_de_Marseille_%28logo%29.svg.png" alt="logo ... partenaires">
+                    <img class="logo-partenaires" src="https://upload.wikimedia.org/wikipedia/fr/thumb/8/82/Ville_de_Marseille_%28logo%29.svg/1280px-Ville_de_Marseille_%28logo%29.svg.png" alt="logo ... partenaires">
+                    <img class="logo-partenaires" src="https://upload.wikimedia.org/wikipedia/fr/thumb/8/82/Ville_de_Marseille_%28logo%29.svg/1280px-Ville_de_Marseille_%28logo%29.svg.png" alt="logo ... partenaires">
+                    <img class="logo-partenaires" src="https://upload.wikimedia.org/wikipedia/fr/thumb/8/82/Ville_de_Marseille_%28logo%29.svg/1280px-Ville_de_Marseille_%28logo%29.svg.png" alt="logo ... partenaires">
                 </div>
                 
                 <div id="centre-IME">
-                    <h3 id="titre-IME"></h3>Vous êtes centre IME
+                    <h3 id="titre-IME">Vous êtes centre IME</h3>
                     <p class="paragraphes-index">
                         Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset 
                         contenant des passages du Lorem Ipsum, et, plus récemment, 
