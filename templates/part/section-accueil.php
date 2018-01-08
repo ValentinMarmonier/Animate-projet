94f5e982f9f24b5c484a944eab15099a2732aafc
Thibaut Vivant <thibaut.vivant@gmail.com>
2018-01-08 13:21:30 +0000

    debut preuve social

--- templates/part/section-accueil.php
+++ templates/part/section-accueil.php
@@ -4,5 +4,6 @@ require_once("$cheminPart/header.php");
 require_once("$cheminPart/section-video.php");
 require_once("$cheminPart/section-fondateurs.php");
 require_once("$cheminPart/section-benefices.php");
+require_once("$cheminPart/section-preuve-sociale.php");
 require_once("$cheminPart/footer.php");
     
---
+++ templates/part/section-preuve-sociale.php
@@ -0,0 +1,35 @@
+<section id="preuves" class="sections-index">
+                <div class="entete">
+                    <h2 class="titre-2-sections">Preuves Sociales</h2>
+                    </div>
+                <div class="big-fact">
+                    <div class="bloctexte" id="apostrophes">
+                    <p class="paragraphes-preuves" lass="paragraphes-index"> 
+                    Il n'a pas fait que survivre cinq siècles,<br>
+                    mais s'est aussi adapté à la bureautique informatique,<br> 
+                    sans que son contenu n'en soit modifié</p>
+                    <p class="signatures">Nom Prenom<br>Metier</p>
+                    </div>
+                    <img class="icones-benefices" src="../src/assets/img/attention-aux-autres.png" alt="">
+                </div>
+                <div class="big-fact">
+                   <img class="icones-benefices" src="../src/assets/img/attention.png" alt="">
+                    <div class="bloctexte" id="apostrophes">
+                    <p class="paragraphes-preuves" lass="paragraphes-index"> 
+                    Il n'a pas fait que survivre cinq siècles,<br>
+                    mais s'est aussi adapté à la bureautique informatique,<br> 
+                    sans que son contenu n'en soit modifié</p>
+                    <p class="signatures">Nom Prenom<br>Metier</p>
+                    </div>
+                </div>
+                <div class="big-fact">
+                    <div class="bloctexte" id="apostrophes">
+                    <p class="paragraphes-preuves" class="paragraphes-index"> 
+                    Il n'a pas fait que survivre cinq siècles,<br>
+                    mais s'est aussi adapté à la bureautique informatique,<br> 
+                    sans que son contenu n'en soit modifié</p>
+                    <p class="signatures">Nom Prenom<br>Metier</p>
+                    </div>
+                    <img class="icones-benefices" src="../src/assets/img/attention-1.png" alt="">
+                </div>
+            </section>
