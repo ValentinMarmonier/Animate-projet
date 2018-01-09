2bbd8c5d942f8b1ba4a589c0aae8d37e0f53cbdc
Dominique GENTY <dominiquegenty.philantroc@gmail.com>
2018-01-09 13:13:05 +0000

    mal assets

--- templates/part/section-video.php
+++ templates/part/section-video.php
@@ -5,7 +5,7 @@
 
 <section>
     <div class="intro">
-    <img class="gif" src="../src/assets/img/coeur.gif" loop=infinite></img>
+    <img class="image-video" src="assets/img/BD AniMate-video.jpg" alt="vignette figurine Animate">
         <div class="video">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/qskzSUsuLeU"
             frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
