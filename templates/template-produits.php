c1c95d19dc9259fc66a6ae72cc06599ef5a9dac4
Thibaut Vivant <thibaut.vivant@gmail.com>
2018-01-10 15:01:38 +0000

    test rotateimg

--- templates/template-produits.php
+++ templates/template-produits.php
@@ -3,4 +3,5 @@
 require_once("$cheminPart/header.php");
 require_once("$cheminPart/section-troubles.php");
 require_once("$cheminPart/section-emotions.php");
+require_once("$cheminPart/section-temoignages-produits.php");
 require_once("$cheminPart/footer.php");
